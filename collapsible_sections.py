b83bac70738dca79ef744e37488cdff03615ec95
ljbred08 <link@brown.fm>
2025-01-02 09:41:31 -0500

    Added collapsible section with extra options. Exit button now stops a running command.

---
+++ collapsible_sections.py
@@ -0,0 +1,53 @@
+import PySimpleGUI as sg
+class CollapsibleSection:
+    def __init__(self, layout, title, key, text_color='white', default_state=False):
+        """
+        Creates a collapsible section for PySimpleGUI windows
+        
+        Args:
+            layout: The layout to be contained in the collapsible section
+            title: Title text shown in the header
+            key: Unique key for this section
+            text_color: Color for the header text and symbol
+            default_state: True for initially expanded, False for initially collapsed
+        """
+        self.SYMBOL_UP = '▸'
+        self.SYMBOL_DOWN = '▾'
+        self.key = key
+        self.title = title
+        self.layout = layout
+        self.text_color = text_color
+        self.default_state = default_state
+        
+    def _collapse(self, layout):
+        """Creates a pinned column that can be made visible/invisible"""
+        col = sg.pin(sg.Column(layout, key=f'{self.key}-CONTENT-', visible=self.default_state))
+        return col
+        
+    def get_layout(self):
+        """Returns the complete layout for the collapsible section"""
+        return [
+            [
+                sg.T(self.SYMBOL_DOWN if self.default_state else self.SYMBOL_UP, 
+                     enable_events=True, k=f'{self.key}-SYMBOL-', 
+                     text_color=self.text_color),
+                sg.T(self.title, enable_events=True, 
+                     text_color=self.text_color, k=f'{self.key}-TITLE-')
+            ],
+            [self._collapse(self.layout)]
+        ]
+
+def handle_section_events(window, event, section):
+    """Handles events for a collapsible section"""
+    if event.startswith(f'{section.key}-SYMBOL-') or event.startswith(f'{section.key}-TITLE-'):
+        is_open = window[f'{section.key}-SYMBOL-'].get() == section.SYMBOL_DOWN
+        window[f'{section.key}-SYMBOL-'].update(
+            section.SYMBOL_UP if is_open else section.SYMBOL_DOWN)
+        window[f'{section.key}-CONTENT-'].update(visible=not is_open)
+        return True
+    return False
+
+def handle_sections_events(window, event, sections):
+    for section in sections:
+        if handle_section_events(window, event, section):
+            break
--- gopro_dashboard_overlay_GUI.py
+++ gopro_dashboard_overlay_GUI.py
@@ -1,19 +1,95 @@
 import PySimpleGUI as sg
 import subprocess
 import os
+from collapsible_sections import CollapsibleSection
+import collapsible_sections as cs
+import sys
+import shutil
 
 # Set the output file suffix
 OUTPUT_SUFFIX = "_dashboard"
 
 size = (40, None)
 
+section1_options = [
+    {'label': 'Generate', 'key': 'generate', 'options': ['default', 'overlay', 'none'], 'default_value': 'default', 'tooltip': 'Type of output to generate. Overlay gives only the widgets with no video.'},
+    {'label': 'Font', 'key': 'font', 'options': ['calibri', 'cambria', 'segoe ui', 'segoe print', 'liberation sans'], 'default_value': 'calibri'},
+    {'label': 'Overlay Size', 'key': 'overlay_size', 'tooltip': '<XxY> e.g. 1920x1080 Force size of overlay. Use if video differs from supported bundled \noverlay sizes (1920x1080, 3840x2160), Required if --use-gpx-only (default: None).'},
+    {'label': 'Background', 'key': 'bg', 'tooltip': 'Background Colour - R,G,B,A - each 0-255, no spaces! (default: (0, 0, 0, 0))', 'default_value': ''},
+    {'label': 'Privacy', 'key': 'privacy', 'tooltip': 'Set privacy zone (lat,lon,km) (default: None)'},
+    {'label': 'Config Dir', 'key': 'config_dir', 'tooltip': f'Location of config files (api keys, profiles, ...) (default: {os.path.expanduser("~")}\.gopro-graphics)', 'default_value': ''},
+    {'label': 'Cache Dir', 'key': 'cache_dir', 'tooltip': f'Location of caches (map tiles, ...) (default: {os.path.expanduser("~")}\.gopro-graphics)', 'default_value': ''},
+    {'label': 'FFMPEG DIR', 'key': 'ffmpeg_dir', 'tooltip': 'Directory where ffmpeg/ffprobe located, default=Look in PATH (default: None)', 'default_value': ''},
+    {'label': 'GPX/FIT', 'key': 'gpx_fit', 'tooltip': 'Use GPX/FIT file for location / alt / hr / cadence / temp ... (default: None)', 'default_value': '', 'type': 'FileBrowse'},
+    {'label': 'GPX Merge', 'key': 'gpx_merge', 'tooltip': '{EXTEND,OVERWRITE} When using GPX/FIT file - OVERWRITE=replace GPS/alt from GoPro with GPX values,\nEXTEND=just use additional values from GPX/FIT file e.g. hr/cad/power (default: MergeMode.EXTEND)', 'default_value': '', 'options': ['EXTEND', 'OVERWRITE']},
+    {'label': 'Use GPX/FIT only', 'key': 'use_gpx_fit_only', 'tooltip': '--use-gpx-only, --use-fit-only\nUse only the GPX/FIT file - no GoPro location data (default: False)', 'default_value': '', 'options': ['', 'GPX', 'FIT']},
+    {'label': 'Map API Key', 'key': 'map_api_key', 'tooltip': 'API Key for map provider, if required (default OSM doesn\'t need one) (default: None)', 'default_value': ''},
+    {'label': 'Layout', 'key': 'layout', 'tooltip': '--layout {default,speed-awareness,xml}\nChoose graphics layout (default: default)', 'default_value': 'default', 'options': ['default', 'speed-awareness', 'xml']},
+    {'label': 'Layout XML', 'key': 'layout_xml', 'tooltip': 'Use XML File for layout (default: None)', 'default_value': '', 'type': 'FileBrowse'},
+    {'label': 'Exclude', 'key': 'exclude', 'tooltip': 'Exclude named component (will include all others) (default: None)', 'default_value': '', 'options': [
+        'asi',
+        'bar',
+        'chart',
+        'compass',
+        'compass_arrow',
+        'gps',
+        'gradient_bar',
+        'info',
+        'map',
+        'msi',
+        'profile',
+        'text',
+    ]},
+    {'label': 'Include', 'key': 'include', 'tooltip': '--include INCLUDE [INCLUDE ...]\nInclude named component (will exclude all others) (default: None)', 'default_value': '', 'options': [
+        'asi',
+        'bar',
+        'chart',
+        'compass',
+        'compass_arrow',
+        'gps',
+        'gradient_bar',
+        'info',
+        'map',
+        'msi',
+        'profile',
+        'text',
+    ]},
+    {'label': 'Units Speed', 'key': 'units_speed', 'tooltip': '--units-speed UNITS_SPEED\nDefault unit for speed. Many units supported: mph, mps, kph, knot, ... (default: mph)', 'default_value': 'mph', 'options': ['mph', 'mps', 'kph', 'knot', 'foot/second', 'metre/second', 'kilometre/hour', 'yard/second', 'inch/second', 'furlong/fortnight']},
+    {'label': 'Units Altitude', 'key': 'units_altitude', 'tooltip': '--units-altitude UNITS_ALTITUDE\nDefault unit for altitude. Many units supported: foot, mile, metre, meter, parsec, angstrom, ... (default: foot)', 'default_value': 'foot', 'options': ['foot', 'mile', 'metre', 'meter', 'parsec', 'angstrom', 'astronomical unit', 'light year', 'nautical mile']},
+    {'label': 'Units Distance', 'key': 'units_distance', 'tooltip': '--units-distance UNITS_DISTANCE\nDefault unit for distance. Many units supported: mile, km, foot, nmi, meter, metre, parsec, ... (default: mile)', 'default_value': 'mile', 'options': ['mile', 'km', 'foot', 'nmi', 'meter', 'metre', 'parsec']},
+    {'label': 'Units Temperature', 'key': 'units_temperature', 'tooltip': '--units-temperature {kelvin,degC,degF}\nDefault unit for temperature (default: degF)', 'default_value': 'degF', 'options': ['kelvin', 'degC', 'degF']},
+    {'label': 'GPS Speed Max', 'key': 'gps_speed_max', 'tooltip': '--gps-speed-max GPS_SPEED_MAX\nMax GPS Speed - Points with greater speed will be considered \'Not Locked\' (default: 60)', 'default_value': '60', 'options': []},
+
+]
+
+section1_layout = [
+    [
+        sg.Text(option['label'], tooltip=option.get('tooltip')),
+        sg.Push(),
+        {
+            'Text': lambda: sg.InputText(key=option['key'], default_text=option.get('default_value'), size=size, tooltip=option.get('tooltip')),
+            'Combo': lambda: sg.Combo(option.get('options', []), key=option['key'], default_value=option.get('default_value'), size=size, tooltip=option.get('tooltip')),
+            'FileBrowse': lambda: sg.Input(key=option['key'], enable_events=True, size=size),
+        }.get(option.get('type'), lambda: sg.Text(f'Unknown type: {option.get("type")}'))()
+        if option.get('type')
+        else (
+            sg.Combo(option.get('options', []), key=option['key'], default_value=option.get('default_value'), size=size, tooltip=option.get('tooltip'))
+            if option.get('options')
+            else sg.InputText(key=option['key'], default_text=option.get('default_value'), size=size, tooltip=option.get('tooltip'))
+        )
+    ] + ([sg.FileBrowse(file_types=(('MP4 Files', '*.mp4'),))] if option.get('type') == 'FileBrowse' else [])
+    for option in section1_options
+]
+section1 = CollapsibleSection(section1_layout, 'Advanced', '-SEC1-')
+
+
 # Define the layout
 layout = [
     [sg.Text('Input Video File  \u2009'), sg.Input(key='input_video', enable_events=True, size=size), sg.FileBrowse(file_types=(('MP4 Files', '*.mp4'),))],
     [sg.Text('Suffix                 '), sg.InputText(key='output_suffix', default_text=OUTPUT_SUFFIX, enable_events=True, size=size)],
     [sg.Text('Output Video File'), sg.Input(key='output_video',  size=size), sg.SaveAs(file_types=(('MP4 Files', '*.mp4'),))],
-    [sg.Text('Font                  '), sg.Combo(['Calibri', 'Cambria', 'Segoe UI', 'Segoe Print', 'Liberation Sans'], key='font', default_value='Calibri', size=size)],  
     [sg.Text('Map Style          '), sg.Combo([
+        'local',
         'cyclosm',
         'geo-dark-matter-brown',
         'geo-dark-matter-dark-grey',
@@ -33,7 +109,6 @@ layout = [
         'geo-positron',
         'geo-toner-grey',
         'geo-toner',
-        'local',
         'osm',
         'tf-atlas',
         'tf-cycle',
@@ -46,22 +121,36 @@ layout = [
         'tf-transport-dark',
         'tf-transport'
     ], key='map_style', default_value='local', size=size)],
-    [sg.Button('Process'), sg.Button('Exit')],
+    [section1.get_layout()],
+    [sg.Button('Process'), sg.Button('Exit',tooltip='Exit the application. If command is running, it will be stopped instead of closing the window.')],
     [sg.Output(size=(80, 20), key='output_console')]
 ]
 
 # Create the window
-window = sg.Window('GoPro Dashboard Overlay GUI', layout)
+window = sg.Window('GoPro Dashboard Overlay GUI', layout, resizable=True)
 
 # Function to run a command and display output in real-time
 def run_command(cmd, window):
-    print(f"Running command: {' '.join(cmd)}\n")
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-    for line in p.stdout:
-        print(line, end='')  # Print to the PySimpleGUI Output element
-    retval = p.wait()
-    print(f"Command exited with return code {retval}")
-    return retval
+    try:
+        print(f"Running command: {' '.join(cmd)}\n")
+        p = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        for line in p.stdout:
+            print(line, end='')  # Print to the PySimpleGUI Output element
+            # Non-blocking read of the window
+            event, values = window.read(timeout=0, timeout_key='-TIMED_OUT-')
+            if event == '-TIMED_OUT-':
+                continue
+            if event == sg.WINDOW_CLOSED or event == 'Exit':
+                p.kill()
+                print("Command interrupted by user.")
+                return p.wait()
+
+        returncode = p.wait()
+        print(f"Command exited with return code {returncode}")
+        return returncode
+    except Exception as e:
+        print(f"Error running command: {e}")
+        return None
 
 # Event loop
 while True:
@@ -69,6 +158,11 @@ while True:
     if event == sg.WINDOW_CLOSED or event == 'Exit':
         break
     
+    try:
+        cs.handle_section_events(window, event, section1)
+    except Exception as e:
+        print(f"Error handling section events: {e}")
+
     if event == 'input_video' or event == 'output_suffix':
         OUTPUT_SUFFIX = values['output_suffix']
         try:
@@ -86,13 +180,11 @@ while True:
         font = values['font'].lower()
 
         # Construct the command
-        cmd = ['gopro-dashboard.py', f"{input_video}", f"{output_video}"]
+        cmd = [sys.executable,shutil.which('gopro-dashboard.py'), f"{input_video}", f"{output_video}"]
 
-        if map_style:
-            cmd.extend(['--map-style', map_style])
-        
-        if font:
-            cmd.extend(['--font', font])
+        for key, value in values.items():
+            if key not in ['input_video', 'output_video', 'output_suffix', 'Browse'] and value:
+                cmd.extend([f"--{key.replace('_', '-')}",value])
 
         # Execute the command and display output in the terminal
         run_command(cmd, window)
